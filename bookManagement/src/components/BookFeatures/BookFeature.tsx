fc93bfe44ef3f4fac5f216087f63e068d16c2ff4
xinyuji <quennel1115@gmail.com>
2024-04-24 21:35:06 +0800

    feat: add book lists and model

--- bookManagement/src/components/BookFeatures/BookFeature.tsx
+++ bookManagement/src/components/BookFeatures/BookFeature.tsx
@@ -1,6 +1,27 @@
 import {Box} from "@mui/material";
+import BookList from "@site/src/components/BookFeatures/BookList";
+import {Book} from "@site/src/components/BookFeatures/BookModel";
+import {useState} from "react";
 
 const BookFeature = () => {
-    return(<Box>123</Box>)
+    const mockBook: Book = {
+        id: '这是id',
+        title: '这是一本书',
+        author: '这是一个作者',
+        isbn: '这是一个isbn',
+        publishedDate: '这是一个发布日期',
+    }
+    const [books, setBooks] = useState<Book[]>( [mockBook, mockBook])
+    const deleteBook = (id: string) => {
+        console.log(id)
+    }
+    const editBook = (book: Book) => {
+        console.log(book.id)
+    }
+    return(
+        <Box>
+            123
+            <BookList books={books} deleteBook={deleteBook} editBook={editBook}/>
+        </Box>)
 }
 export default BookFeature
---
+++ bookManagement/src/components/BookFeatures/BookList.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Stack, ListItem, List, Typography, ListItemButton, ListItemIcon, IconButton, Paper } from '@mui/material';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import DeleteIcon from '@mui/icons-material/Delete';
+const BookList = ({ books, deleteBook, editBook }) => {
+    return (
+        <Paper elevation={6} square={false} sx={{ bgcolor: 'background.paper' }}>
+            <List sx={{ p: 1, width: '100%' }}>
+                {books.map((book) => {
+                    return (
+                        <BookListItem
+                            book={book}
+                            deleteBook={deleteBook}
+                            editBook={editBook}
+                        />
+                    );
+                })}
+            </List>
+        </Paper>
+    );
+};
+
+const BookListItem = ({ book, deleteBook, editBook }) => {
+    return (
+        <ListItem
+            key={book.id}
+            divider={true}
+            sx={{ width: '100%', margin: 'auto', display: 'flex', justifyContent: 'space-around', border: '1px solid light-gray' }}
+            secondaryAction={
+                <Stack direction="row" spacing={2}>
+                    <IconButton edge="end" aria-label="edit" onClick={() => editBook(book)} >
+                        <ModeEditIcon />
+                    </IconButton>
+                    <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={() => deleteBook(book.id)}
+                    >
+                        <DeleteIcon />
+                    </IconButton>
+                </Stack>
+            }
+            disablePadding
+        >
+            <ListItemButton>
+                <Stack spacing={{ xs: 1, sm: 2 }} direction="row" useFlexGap flexWrap="wrap">
+                    <Typography
+                        sx={{ width: '70%' }}
+                        style={{ color: 'black' }}
+                        key={book.id}
+                    >
+                        Title: {book.title}
+                    </Typography>
+                    <Typography
+                        sx={{ width: '70%' }}
+                        style={{ color: 'black' }}
+                        key={book.id}
+                    >
+                        Author: {book.author}
+                    </Typography>
+                    <Typography
+                        sx={{ width: '70%' }}
+                        style={{ color: 'black' }}
+                        key={book.id}
+                    >
+                        ISBN: {book.isbn}
+                    </Typography>
+                    <Typography
+                        sx={{ width: '70%' }}
+                        style={{ color: 'black' }}
+                        key={book.id}
+                    >
+                        publishedDate: {book.publishedDate}
+                    </Typography>
+                </Stack>
+
+            </ListItemButton>
+        </ListItem>
+    );
+};
+export default BookList;
